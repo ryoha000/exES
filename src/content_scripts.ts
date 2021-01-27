473b4885085481f14b26320ba98f0ebd6b6cb1dd
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2021-01-27 19:40:47 +0900

    挿入位置の決定

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -1,4 +1,16 @@
 window.onload = function() {
-  console.log('test');
-  console.log(location)
+  insertElement()
+}
+
+const BEFORE_ELEMENT_ID = "image_and_basic_infomation"
+const insertElement = () => {
+  const beforeElement = document.getElementById(BEFORE_ELEMENT_ID)
+  if (!beforeElement) {
+    console.error('批評空間の仕様が変わりました。@ryoha000 に報告していただければ幸いです。')
+    return
+  }
+
+  const container = document.createElement('div')
+  container.innerHTML = 'container'
+  beforeElement.parentNode?.insertBefore(container, beforeElement.nextElementSibling)
 }
