e575677f9001fe104f085e91c165f8738fabf3e7
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2021-01-27 18:16:06 +0900

    use webpack

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
+const config = () => {
+  return {
+    entry: {
+      content_scripts: path.join(__dirname, 'src', 'content_scripts.ts')
+    },
+    output: {
+      // distディレクトリにcontent_scripts.jsを吐く
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
+          use: 'ts-loader',
+          exclude: '/node_modules/'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    plugins: [
+      // publicディレクトリにあるファイルをdistディレクトリにコピーする
+      new CopyWebpackPlugin({ patterns: [{ from: 'public', to: '.' }] })
+    ]
+  }
+}
+
+export default config
