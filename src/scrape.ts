8024fcc63454b314d045fce3298601bec4547a47
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2021-01-28 22:57:06 +0900

    フェイルオーバーに上限回数を設定

--- src/scrape.ts
+++ src/scrape.ts
@@ -33,32 +33,42 @@ interface AmazonResponse {
 }
 
 export const getAmazonPrices = async (urls: URL[]): Promise<AmazonResponse[]> => {
+  let tryCount = 0
+  const requestAmazonPrice = async (asin: string): Promise<AmazonResponse> => {
+    try {
+      const res = await fetch(`http://localhost:3000/amazon`, {
+        method: "POST",
+        body: JSON.stringify({ asin: asin })
+      })
+      if (!res.ok) {
+        throw new Error(await res.text())
+      }
+      const text = await res.text()
+      return JSON.parse(text) as AmazonResponse
+    } catch (e) {
+      tryCount++
+      console.error(e)
+      if (tryCount > 5) {
+        throw new Error("over 5 fail")
+      }
+      await sleep(1000)
+      return await requestAmazonPrice(asin)
+    }
+  }
+
   const res: AmazonResponse[] = []
   for (const url of urls) {
+    tryCount = 0
     const asin = getASINFromAmazonURL(url)
     if (asin.length === 0) {
       res.push({ price: 0, title: "" })
     }
-    // サーバーからAmazonに同時にリクエストを送りたくないからわざと直列にしてる
-    res.push(await requestAmazonPrice(asin))
+    try {
+      // サーバーからAmazonに同時にリクエストを送りたくないからわざと直列にしてる
+      res.push(await requestAmazonPrice(asin))
+    } catch (e) {
+      console.error(e)
+    }
   }
   return res
-}
-
-const requestAmazonPrice = async (asin: string): Promise<AmazonResponse> => {
-  try {
-    const res = await fetch(`http://localhost:3000/amazon`, {
-      method: "POST",
-      body: JSON.stringify({ asin: asin })
-    })
-    if (!res.ok) {
-      throw new Error(await res.text())
-    }
-    const text = await res.text()
-    return JSON.parse(text) as AmazonResponse
-  } catch (e) {
-    console.error(e)
-    await sleep(1000)
-    return await requestAmazonPrice(asin)
-  }
 }
