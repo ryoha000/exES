ec13b0e7e5646e1971173a09d41af6ab84129c24
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2021-01-29 17:18:16 +0900

    storeからRowInfoに変換する関数の追加

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { RowInfo } from "./components/table_row"
+import { PriceInfo, SaleInfo } from "./store"
+
 export const getASINFromAmazonURL = (url: URL) => {
   const paths = url.pathname.split("/")
   const beforeTargetIndex = paths.findIndex(v => v === "ASIN")
@@ -13,3 +16,28 @@ export const getDlsiteIDFromURL = (url: URL) => {
 }
 
 export const sleep = (msec: number) => new Promise<void>(resolve => setTimeout(resolve, msec));
+
+export const convertPriceInfosToRowInfos = (pis: PriceInfo[]) => {
+  const rowInfos: RowInfo[] = []
+  for (const pi of pis) {
+    rowInfos.push([
+      { text: pi.title, url: pi.titleURL }
+    ],[
+      { text: pi.price, url: pi.priceURL }
+    ])
+  }
+  return rowInfos
+}
+
+export const convertSaleInfosToRowInfos = (sis: SaleInfo[]) => {
+  const rowInfos: RowInfo[] = []
+  for (const si of sis) {
+    const day = `${si.start.slice(0, 10)} ～ ${si.end.slice(0, 10)}`
+    rowInfos.push([
+      { text: day }
+    ],[
+      { text: si.content }
+    ])
+  }
+  return rowInfos
+}
